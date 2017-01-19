945d5239904e2246cf8994241b1a06da6597ef53
TomRobot <thomasrobson@Thomass-MacBook-Pro-2.local>
2017-01-18 20:24:49 -0700

    Change to Camping
    Fix Google Map, change to camping, remove background image.

---
+++ main.js
@@ -0,0 +1,6 @@
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10,
+            center: new google.maps.LatLng(40.0150, -105.2705),
+        });
+    }
